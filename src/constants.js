e4cfe12473f5f8408a259f948994565c385ac71a
Jashwanth <jashukurma@gmail.com>
2024-09-17 00:35:54 +0530

    added default image url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 export const DB_NAME = "medsvtestdb";
 export const FILE_STORAGE_DESTINATION = "./public/temp";
 export const API_BASE_URL = "/api/v1";
+export const DEFAULT_IMAGE_URL =
+  "http://res.cloudinary.com/twitch-cloud/image/upload/v1726503631/tsvcngqnetolxcts8kp8.webp";
